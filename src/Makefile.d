36c7d0e13d25430daa98807c7f56df43fb0b0dd6
sadko4u <sadko4u@gmail.com>
2020-10-08 16:03:01 +0300

    Updated dependencies

--- src/Makefile.d
+++ src/Makefile.d
@@ -1,10 +1,10 @@
-$(LSP_R3D_GLX_LIB_BIN)/main/builtin.o: main/builtin.cpp \
- $(LSP_R3D_GLX_LIB_INC)/lsp-plug.in/r3d/iface/builtin.h \
- $(LSP_R3D_GLX_LIB_INC)/lsp-plug.in/r3d/iface/factory.h \
- $(LSP_R3D_GLX_LIB_INC)/lsp-plug.in/r3d/iface/version.h \
- $(LSP_R3D_GLX_LIB_INC)/lsp-plug.in/r3d/iface/backend.h \
- $(LSP_R3D_GLX_LIB_INC)/lsp-plug.in/r3d/iface/types.h \
+$(LSP_R3D_IFACE_BIN)/main/builtin.o: main/builtin.cpp \
+ $(LSP_R3D_IFACE_INC)/lsp-plug.in/r3d/iface/builtin.h \
+ $(LSP_R3D_IFACE_INC)/lsp-plug.in/r3d/iface/factory.h \
+ $(LSP_R3D_IFACE_INC)/lsp-plug.in/r3d/iface/version.h \
+ $(LSP_R3D_IFACE_INC)/lsp-plug.in/r3d/iface/backend.h \
+ $(LSP_R3D_IFACE_INC)/lsp-plug.in/r3d/iface/types.h \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/types.h \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/version.h \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/status.h
-$(LSP_R3D_GLX_LIB_BIN)/test/main.o: test/main.cpp
+$(LSP_R3D_IFACE_BIN)/test/main.o: test/main.cpp
