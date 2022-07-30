16a9b52489e552dad77b9bf2d3a3bd6e71b812e3
Vladimir Sadovnikov <sadko4u@gmail.com>
2022-07-31 01:03:38 +0300

    Updated build

--- src/Makefile.d
+++ src/Makefile.d
@@ -14,4 +14,7 @@ $(LSP_R3D_IFACE_BIN)/main/builtin.o: main/builtin.cpp \
  $(LSP_R3D_IFACE_INC)/lsp-plug.in/r3d/iface/backend.h \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/status.h \
  $(LSP_R3D_IFACE_INC)/lsp-plug.in/r3d/iface/types.h
-$(LSP_R3D_IFACE_BIN)/test/main.o: test/main.cpp
+$(LSP_R3D_IFACE_BIN)/test/main.o: test/main.cpp \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/main.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/version.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/types.h
