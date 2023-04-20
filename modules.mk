c44c8d5e2417bc4314478ebbba9ed52288e21dc2
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-04-20 20:47:39 +0300

    Updated module versions in dependencies

--- modules.mk
+++ modules.mk
@@ -20,7 +20,7 @@
 
 #------------------------------------------------------------------------------
 # Variables that describe source code dependencies
-LSP_COMMON_LIB_VERSION     := 1.0.26
+LSP_COMMON_LIB_VERSION     := 1.0.27
 LSP_COMMON_LIB_NAME        := lsp-common-lib
 LSP_COMMON_LIB_TYPE        := src
 LSP_COMMON_LIB_URL_RO      := https://github.com/lsp-plugins/$(LSP_COMMON_LIB_NAME).git
